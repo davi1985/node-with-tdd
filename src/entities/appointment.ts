658a6a50ebefd4d80b38227688adab3151869f63
Davi Silva <davisilvaphoto@gmail.com>
2022-09-08 20:26:07 -0300

    feat: appointments entity

---
+++ src/entities/appointment.ts
@@ -0,0 +1,39 @@
+export interface AppointmentProps {
+  customer: string;
+  startsAt: Date;
+  endsAt: Date;
+}
+
+export class Appointment {
+  private props: AppointmentProps;
+
+  constructor(props: AppointmentProps) {
+    const { startsAt, endsAt } = props;
+
+    this.validateDates(startsAt, endsAt);
+
+    this.props = props;
+  }
+
+  get customer(): string {
+    return this.props.customer;
+  }
+
+  get startsAt(): Date {
+    return this.props.startsAt;
+  }
+
+  get endsAt(): Date {
+    return this.props.endsAt;
+  }
+
+  private validateDates(startsAt: Date, endsAt: Date) {
+    if (startsAt < new Date()) {
+      throw new Error("Invalid start date");
+    }
+
+    if (endsAt <= startsAt) {
+      throw new Error("Invalid end date");
+    }
+  }
+}
