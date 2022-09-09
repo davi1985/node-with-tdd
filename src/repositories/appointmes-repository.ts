9768163b27d0a6a5a1f771a50b5c491e008130aa
Davi Silva <davisilvaphoto@gmail.com>
2022-09-09 19:57:46 -0300

    feat: appointments-repository and in-memory-appointments-repository

---
+++ src/repositories/appointmes-repository.ts
@@ -0,0 +1,10 @@
+import { Appointment } from "../entities/appointment";
+
+export interface AppointmentRepository {
+  create(appointment: Appointment): Promise<void>;
+
+  findOverlappingAppointment(
+    startsAt: Date,
+    endsAt: Date
+  ): Promise<Appointment | null>;
+}
