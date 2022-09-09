2dbfb1954d71d182ff366123af511aae3f71670a
Davi Silva <davisilvaphoto@gmail.com>
2022-09-09 19:59:07 -0300

    feat: create-appointments

---
+++ src/use-cases/create-appointment.ts
@@ -0,0 +1,40 @@
+import { Appointment } from "../entities/appointment";
+import { AppointmentRepository } from "../repositories/appointmes-repository";
+
+interface CreateAppointmentRequest {
+  customer: string;
+  startsAt: Date;
+  endsAt: Date;
+}
+
+type CreateAppointmentResponse = Appointment;
+
+export class CreateAppointment {
+  constructor(private appointmentsRepository: AppointmentRepository) {}
+
+  async execute({
+    customer,
+    startsAt,
+    endsAt,
+  }: CreateAppointmentRequest): Promise<CreateAppointmentResponse> {
+    const overlappingAppointment =
+      await this.appointmentsRepository.findOverlappingAppointment(
+        startsAt,
+        endsAt
+      );
+
+    if (overlappingAppointment) {
+      throw new Error("Another appointment overlaps this appointmet dates.");
+    }
+
+    const appointment = new Appointment({
+      customer,
+      startsAt,
+      endsAt,
+    });
+
+    await this.appointmentsRepository.create(appointment);
+
+    return appointment;
+  }
+}
